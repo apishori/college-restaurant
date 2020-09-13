b90dc08cc1efa6d4220d909c0fa154b5c9c99eac
ArmaanP <pishori@usc.edu>
2020-09-13 14:16:42 -0700

    Setup users and orders app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from phone_field import PhoneField
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=15, blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=255, blank=False, null=False, unique=True)
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Profile(models.Model):
+    phone = PhoneField(blank=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            return
+            #can do something with data 
+            #return redirect()
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
